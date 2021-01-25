7c0101f59e558951f9f3940c0b6fad4264205f88
JungHo <jungpark007@naver.com>
2021-01-25 09:12:48 +0900

    스프링 데이터 jpa 셋팅

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,29 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberJpaRepositoryTest {
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertEquals(savedMember.getId(), member.getId());
+        assertEquals(savedMember.getUsername(), member.getUsername());
+    }
+}
